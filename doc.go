c4b2dbc3b36352c6e91b2b7b577c7a63b3be3d87
Joel Cornett <joecorne@amazon.com>
2017-09-02 19:17:43 -0400

    update doc

--- doc.go
+++ doc.go
@@ -4,10 +4,10 @@ This package supports 'mad-lib' style template matching on strings.
 
 The list of format control sequences is as follows:
 
-- %(name)s: a named capture group using the verb 's'
-- %()s: an anonymous capture group using the verb 's'
-- %s: an non-capturing group using the verb 's'
-- %%: the literal character '%'
+	- %(name)s: a named capture group using the verb 's'
+	- %()s: an anonymous capture group using the verb 's'
+	- %s: an non-capturing group using the verb 's'
+	- %%: the literal character '%'
 
 */
 package patmatch
